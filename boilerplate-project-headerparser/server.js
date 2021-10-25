f26cbb2cb297abd0f38cef77bb7f7dcfb53858c4
Bilal <bilalahmed90@yahoo.com>
2021-10-25 18:04:44 +0100

    Get user's ip address and list of languages

--- boilerplate-project-headerparser/server.js
+++ boilerplate-project-headerparser/server.js
@@ -27,8 +27,9 @@ app.get("/api/hello", function (req, res) {
 
 // head parser API endpoint
 app.get("/api/whoami", (req, res) => {
-  res.json({"ipaddress":"159.20.14.100","language":"en-US,en;q=0.5",
-"software":"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0"});
+  let ip = req.ip;
+  let languages = req.acceptsLanguages();  
+  res.json({ipaddress: ip, language: languages, software:""});
 });
 
 // listen for requests :)
