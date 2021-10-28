5fc30e7ff8dbf036d3a11e8dd6dceda9e4f32e7e
Bilal <bilalahmed90@yahoo.com>
2021-10-28 21:53:43 +0100

    Create new user in mongodb from submitted input

--- boilerplate-project-exercisetracker/server.js
+++ boilerplate-project-exercisetracker/server.js
@@ -1,21 +1,36 @@
 const express = require('express')
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true,
-  useUnifiedTopology: true });
+mongoose.connect(process.env['MONGO_URI'], { useNewUrlParser: true,
+useUnifiedTopology: true })
+const personSchema = new mongoose.Schema({ username: 'string' })
+const Person = mongoose.model('Person', personSchema)
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
-});
+})
 
-
+// create new user
+app.post("/api/users", async (req, res) => {
+  const newUser = new Person ({ username: req.body.username })
+  try { 
+    await newUser.save((err, data) => {
+      res.json({ username: data.username, _id: data.id });
+    }); 
+  } catch (err) {
+    console.error(err)
+    res.status[500].json('Server error...');
+  }
+})
 
 
 
